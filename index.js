e0a873e4e0ebdc841ca0d4e94d8c7595df86024c
24kms <comesrv@proton.me>
2024-09-03 23:33:39 +0200

    h

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Analytics } from '@vercel/analytics';
+
+function App() {
+  return (
+    <div>
+      {/* Your other components */}
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
